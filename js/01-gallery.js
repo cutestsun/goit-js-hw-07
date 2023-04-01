7f6933f2dfc24856feeb5239586fe2212c1a2071
Illia Shulha <ilyxa2836@gmail.com>
2023-04-01 19:13:49 +0200

    solved

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+  )
+  .join("");
+
+galleryEl.innerHTML = galleryItemsEl;
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(evt) {
+  const { target } = evt;
+  evt.preventDefault();
+
+  if (!target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  createLightBox(target);
+}
+
+function createLightBox(target) {
+  const instance = basicLightbox.create(
+    `<img src="${target.dataset.source}">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onEscClick);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", onEscClick);
+      },
+    }
+  );
+
+  function onEscClick(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+
+  showLightBox(instance);
+}
+
+function showLightBox(instance) {
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const galleryItemsEl = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`
+  )
+  .join("");
+
+galleryEl.innerHTML = galleryItemsEl;
+
+galleryEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(evt) {
+  evt.preventDefault();
+
+  if (!evt.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  let gallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+  });
+
+  gallery.on("show.simplelightbox");
+}
